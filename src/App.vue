42dde72f5bca73e13f0f6bb6b32e32abffd583bd
Ivan Zhdanov <tain.krios@gmail.com>
2022-08-11 19:09:00 +0200

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen font-Jakarta bg-weather-primary">
+    <SiteNavigation />
     <RouterView />
   </div>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import SiteNavigation from './components/SiteNavigation.vue';
 </script>
-
-<style scoped>
-
-</style>
