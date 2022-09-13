897cd24190aec8445954a958b3bf44299aa10708
tainkrios <tain.krios@gmail.com>
2022-09-13 16:40:38 +0200

    formatt

--- src/components/CityList.vue
+++ src/components/CityList.vue
@@ -16,14 +16,19 @@ import CityCard from './CityCard.vue';
 const savedCities = ref()
 const getCities = async () => {
   if (localStorage.getItem('savedCities')) {
-    savedCities.value = JSON.parse(localStorage.getItem("savedCities") || '{}')
+    savedCities.value = JSON.parse(
+      localStorage.getItem("savedCities") || '{}'
+    )
 
     const requests: any[] = []
     savedCities.value.forEach((city: { coords: { lat: any; lng: any; }; }) => {
       requests.push(
-        axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${city.coords.lat}&lon=${city.coords.lng}&appid=f5352e084d98d5876fd9a6a2504e64e9&units=metric`)
+        axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${city.coords.lat}&lon=${city.coords.lng}&appid=f5352e084d98d5876fd9a6a2504e64e9&units=metric`
+        )
       )
     })
+
     const weatherData = await Promise.all(requests)
 
     await new Promise(res => setTimeout(res, 1000))
@@ -36,12 +41,28 @@ const getCities = async () => {
 await getCities()
 
 const router = useRouter()
-const goToCityView = (city: { state: string; city: string; id: string; coords: { lat: string; lng: string; }; }) => {
+const goToCityView = (city: 
+  { 
+    state: string, 
+    city: string, 
+    id: string, 
+    coords: { 
+      lat: string, 
+      lng: string; 
+    }
+  }
+) => {
   router.push({
     name: 'cityView',
-    params: { state: city.state, city: city.city },
-    query: { id: city.id, lat: city.coords.lat, lng: city.coords.lng }
+    params: { 
+      state: city.state, 
+      city: city.city 
+    },
+    query: { 
+      id: city.id, 
+      lat: city.coords.lat, 
+      lng: city.coords.lng 
+    }
   })
-
 }
 </script>
